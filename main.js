2ad49c4d342b14e72644307acf160ec7236b9604
Krithika <krithikabq@gmail.com>
2022-06-04 01:36:03 +0530

    add oauth

--- main.js
+++ main.js
@@ -2,28 +2,109 @@ console.log("inside main.js");
 
 const express = require("express");
 var bodyParser = require("body-parser");
+const axios = require("axios");
 
 var app = express();
 app.use(bodyParser());
 
 app.use(express.static("static"));
 
-app.get("/", (req, res) => {
-  res.sendFile("./static/index.html");
-});
-
 require("dotenv").config();
 const Advice = require("./models/advice");
+
+let user = {};
+
+const {
+  ClientCredentials,
+  ResourceOwnerPassword,
+  AuthorizationCode,
+} = require("simple-oauth2");
+
+const callbackUrl = "http://localhost:3001/callback";
+
+const client = new AuthorizationCode({
+  client: {
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: "https://recurse.com",
+    tokenPath: "/oauth/token",
+    authorizePath: "/oauth/authorize",
+  },
+  options: {
+    authorizationMethod: "body",
+  },
+});
+
+// Authorization uri definition
+const authorizationUri = client.authorizeURL({
+  redirect_uri: callbackUrl,
+});
+
+// Initial page redirecting to Recurse
+app.get("/auth", (req, res) => {
+  if (!user.id) {
+    console.log(authorizationUri);
+    res.redirect(authorizationUri);
+  } else {
+    res.redirect("/");
+  }
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get("/callback", async (req, res) => {
+  const { code } = req.query;
+  const options = {
+    code,
+    redirect_uri: callbackUrl,
+  };
+
+  try {
+    const accessToken = await client.getToken(options);
+
+    console.log("The resulting token: ", accessToken.token);
+    axios
+      .get("https://recurse.com/api/v1/profiles/me", {
+        headers: { Authorization: `Bearer ${accessToken.token.access_token}` },
+      })
+      .then((res) => {
+        console.log(`statusCode: ${res.status}`);
+        console.log(res);
+        user = res.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    return res.redirect("/");
+  } catch (error) {
+    console.error("Access Token Error", error.message);
+    return res.status(500).json("Authentication failed");
+  }
+});
+
+// app.get("/", (req, res) => {
+//   res.send('Hello<br><a href="/auth">Log in with Recurse</a>');
+// });
+
+app.get("/", (req, res) => {
+  console.log("in /", user.id);
+  if (user.id) {
+    res.sendFile(__dirname + "/static/main.html");
+  } else {
+    res.redirect("/auth");
+  }
+});
 
 app.post("/api/submit", (request, response) => {
   console.log(
     "in submit",
     request.body,
     request.body.content,
-    request.body.description,
-    request.body.author_id,
-    request.body.author_name
+    request.body.description
   );
+  console.log("in submit", user);
   const body = request.body;
 
   if (body.content === undefined) {
@@ -32,8 +113,8 @@ app.post("/api/submit", (request, response) => {
   const a = new Advice({
     content: body.content,
     description: body.description,
-    author_id: body.author_id,
-    author_name: body.author_name,
+    author_id: user.id,
+    author_name: user.name,
   });
 
   a.save().then((savedAdvice) => {
@@ -49,19 +130,19 @@ app.post("/api/handleMessage", (request, response) => {
     request.body.message.sender_id
   );
   let advice;
+  let responsePayload = {
+    response_not_required: true,
+  };
   Advice.aggregate([{ $sample: { size: 1 } }]).then((result) => {
     console.log(result);
     advice = result[0];
 
-    const adviceResponse = {
+    responsePayload = {
       content: `${advice.content} 
       — [${advice.author_name}](https://www.recurse.com/directory/${advice.author_id})`,
     };
-    const responseNotRequiredPayload = {
-      response_not_required: true,
-    };
 
-    response.json(adviceResponse);
+    response.json(responsePayload);
   });
 });
 
--- static/script.js
+++ static/script.js
@@ -1,7 +1,21 @@
-let formElement = document.getElementById("form")
+let formElement = document.getElementById("form");
 
 function onSubmit(e) {
   e.preventDefault();
+
+  fetch("/api/submit", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: document.getElementById("tldr").value,
+      description: document.getElementById("desc").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {});
 }
 
 formElement.onsubmit = onSubmit;
