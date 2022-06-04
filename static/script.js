7ddae0ae10a99f9c07b6edd08002d009dd822cba
Krithika <krithikabq@gmail.com>
2022-06-04 22:52:33 +0530

    make submit button nicer

--- static/script.js
+++ static/script.js
@@ -1,8 +1,17 @@
 let formElement = document.getElementById("form");
+let submitButton = document.getElementById("submit");
+
+function resetSubmitButton() {
+  submitButton.disabled = false;
+  submitButton.innerText = "Submit!";
+}
 
 function onSubmit(e) {
   e.preventDefault();
-
+  let contentElement = document.getElementById("tldr");
+  let descriptionElement = document.getElementById("desc");
+  submitButton.disabled = true;
+  submitButton.innerText = "Submitting";
   fetch("/api/submit", {
     method: "POST",
     headers: {
@@ -10,12 +19,17 @@ function onSubmit(e) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      content: document.getElementById("tldr").value,
-      description: document.getElementById("desc").value,
+      content: contentElement.value,
+      description: descriptionElement.value,
     }),
   })
     .then((response) => response.json())
-    .then((data) => {});
+    .then((data) => {
+      contentElement.value = "";
+      descriptionElement.value = "";
+      submitButton.innerText = "Submitted!";
+      setTimeout(resetSubmitButton, 2000);
+    });
 }
 
 formElement.onsubmit = onSubmit;
