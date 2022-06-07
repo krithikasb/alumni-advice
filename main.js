310cf4384db8dcbd511eead269d683d806a65fd1
Krithika <krithikabq@gmail.com>
2022-06-08 00:52:48 +0530

    add api for sending message

--- main.js
+++ main.js
@@ -13,6 +13,22 @@ require("dotenv").config();
 const Advice = require("./models/advice");
 const Subscriber = require("./models/subscriber");
 
+const zulipInit = require("zulip-js");
+
+let zulipConfig;
+if (process.env.NODE_ENV === "production") {
+  zulipConfig = {
+    username: process.env.ZULIP_USERNAME,
+    apiKey: process.env.ZULIP_API_KEY,
+    realm: process.env.ZULIP_REALM,
+  };
+} else {
+  const path = require("path");
+  const zuliprc = path.resolve(__dirname, "zuliprc");
+  console.log(zuliprc);
+  zulipConfig = { zuliprc };
+}
+
 var session = require("express-session");
 
 const oneDay = 1000 * 60 * 60 * 24;
@@ -246,6 +262,43 @@ app.get("/api/handleMessage", (request, response) => {
     };
 
     response.json(adviceResponse);
+  });
+});
+
+app.get("/api/sendAdvice", (request, response) => {
+  const client = await zulipInit(zulipConfig);
+  // The zulip object now contains the config from the zuliprc file
+  // console.log(await zulip.streams.subscriptions.retrieve());
+
+  // get random advice
+  let result = await Advice.aggregate([{ $sample: { size: 1 } }]);
+  console.log(result);
+  let advice = result[0];
+  let content;
+
+  if (advice.description) {
+    content =
+      `**${advice.content}**\n\n${advice.description}\n\n` +
+      `— [${advice.author_name}](https://www.recurse.com/directory/${advice.author_id})`;
+  } else {
+    content =
+      `${advice.content}\n` +
+      `— [${advice.author_name}](https://www.recurse.com/directory/${advice.author_id})`;
+  }
+
+  let result2 = await Subscriber.find({}, "zulip_id");
+  console.log(result2);
+  // send a message
+  for (let s of result2) {
+    params = {
+      to: [s.zulip_id],
+      type: "private",
+      content: content,
+    };
+    console.log(await client.messages.send(params));
+  }
+  response.json({
+    status: "success"
   });
 });
 
